63190282d8c7192101673ce32df76b60b5556cb2
SteveMCWin <stevp003@gmail.com>
2025-07-29 22:26:35 +0200

    Started working on the Exercise Progress functionality

--- models/create_track_table.sql
+++ models/create_track_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE workout_track_data (
     weight FLOAT, -- NOTE: This doesn't neccessarily have to be the weight that is specified in the ex_day, unless it's null
     set_num INTEGER,
     rep_num INTEGER DEFAULT 0,
+    time_recorded DATE DEFAULT NULL,
     FOREIGN KEY(ex_day) REFERENCES exercise_day(id),
     FOREIGN KEY(track) REFERENCES workout_track(id)
 );
@@ -26,16 +27,3 @@ CREATE TABLE workout_track (
     FOREIGN KEY(usr) REFERENCES users(id),
     UNIQUE(plan, usr, workout_date)
 );
-
-
-
-DROP TABLE IF EXISTS track_exercise;
-
--- NOTE: this table is used to track which ex_days are for which track since ex days can be changed in a plan, but the track needs to remember them
--- CREATE TABLE track_exercise (
---     track INTEGER,
---     ex_day INTEGER,
---     PRIMARY KEY(track, ex_day),
---     FOREIGN KEY(track) REFERENCES workout_track(id),
---     FOREIGN KEY(ex_day) REFERENCES exercise_day(id)
--- );
